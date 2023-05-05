462331a807bb4508a6ed2be275fef325ecffa7a8
TaffyD93 <122776521+TaffyD93@users.noreply.github.com>
2023-05-05 02:38:58 +0100

    Update Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import item1 from '../Images/arsenal-team.png';
-import item2 from '../Images/liverpool-team.jpeg';
-import item3 from '../Images/man-city-team.jpeg';
+import item1 from '../Images/arsenal-team.png'; //https://www.arsenal.com/news/check-out-our-202223-official-mens-team-photo
+import item2 from '../Images/liverpool-team.jpeg'; //https://z-upload.facebook.com/LiverpoolFC/posts/10161576671287573
+import item3 from '../Images/man-city-team.jpeg'; //https://www.mancity.com/news/mens/first-team-official-photo-2022-2023-season-63798226
 
 const slideshowImages = [item1, item2, item3];
 const delay = 2500;
